dc26f874eb84ea36c0cc11a81816b4fc5e3f78a8
edrikL <leyouyuan@126.com>
2017-09-01 17:09:14 +0800

    repo

--- git_commit_push.bat
+++ git_commit_push.bat
@@ -5,16 +5,16 @@ set cur_dir=%cd%
 echo %cur_dir%
 
 
-echo ......................................................
+echo ..........................................
 echo switch to master branch..
 echo checkout mvn_repo..
 
 call git checkout master
 
 echo git commit mvn_repo..
-
+echo ....................................................
 call git add .
 call git commit -m "repo"
 call git push --set-upstream origin master
 
-echo ..................................................END
+echo ...........................................................................END
