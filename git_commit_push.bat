5d48ed5838a9656ae79835677d94c9c63b966400
edrikL <leyouyuan@126.com>
2017-09-01 17:05:28 +0800

    repo

---
+++ git_commit_push.bat
@@ -0,0 +1,17 @@
+
+
+set cur_dir=%cd%
+echo %cur_dir%
+
+echo checkout mvn_repo...............................................................
+
+call git checkout -b master
+
+echo git commit mvn_repo.............................................................
+
+call git add .
+call git commit -m "repo"
+call git push --set-upstream origin master
+
+echo .......................................................................................END.
+
